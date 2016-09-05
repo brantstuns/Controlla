d224c2840fc47b9796f810b6e4ef6f605422e6c3
Brant Stuns <brantstuns@gmail.com>
2016-09-05 14:10:47 -0700

    Externalized controller module for boa page. Successfully hitting the boa page but failing submitting form

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
 'use strict'
 const express = require("express")
 const getLogger = require('./logger');
+const $ = require('jQuery');
+const boaControlla = require('./controllers/boaControlla');
 
 module.exports = {
   getApp: function () {
     const app = express();
-    app.get("/", function (req, res) { res.send("like controlla, controlla, controlla...")});
+    app.get('/boa', boaControlla);
+    app.use((req, res) => { res.send("like controlla, controlla, controlla...") });
     return app;
   },
   start: function (port) {
     const logger = getLogger();
     const app = this.getApp();
     return new Promise((resolve) => server = app.listen(port, () => {
-      logger.info('Server started on port ... ' + port);
+      logger.info('CONTROLLA STARTED ON PORT : ' + port);
     resolve();
       }));
   },
-  stop: function (port) {
+  stop: () => {
     return new Promise((resolve) => server.close(resolve));
   }
 };
